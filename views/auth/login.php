36282fdcd2b0034c3dad4e907e3495aa5852c5b5
aparece1241 <a-harvey@chronostep.com>
2023-03-02 12:47:55 +0800

    update: create login page

--- views/auth/login.php
+++ views/auth/login.php
@@ -17,6 +17,20 @@
                 <input class="input" type="password" placeholder="Password" name="password" id="">
                 <input class="button" type="submit" value="Sign in">
             </form>
+            <p>or</p>
+
+            <div class="social-media google-sign">
+                <img class="social-icons google-icon" src="../../public/icons/google-icon.png" alt="google">
+                <span><b>&nbsp;&nbsp;continue with google</b></span>
+            </div>
+            <div class="social-media facebook-sign">
+                <img class="social-icons facebook-icon" src="../../public/icons/facebook-icon.png" alt="facebook">
+                <span><b>&nbsp;&nbsp;continue with facebook</b></span>
+            </div>
+            <div class="forgot-cont">
+                <a href="#" class="forgot-pass">Forgot password?</a>
+            </div>
+            <p style="margin-top: 22px;">Don't have account yet? <b><a href="#">Sign up</a></b></p>
         </div>
     </div>
 </body>
