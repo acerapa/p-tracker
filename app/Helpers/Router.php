d16edebe1782b77d08701966925e6b1b2bb0deea
projectdev <projectdev751@gmail.com>
2023-03-14 17:01:25 +0800

    extract query parameters

--- app/Helpers/Router.php
+++ app/Helpers/Router.php
@@ -21,13 +21,16 @@ class Router {
 
     /**
      * Calls this method when nothing is specified function name
+     * 
+     * @param String $method static function name
+     * @param Array  $args arguments 
      */
     public static function __callStatic($method, $args)
     {
         if (in_array(strtoupper($method), self::$methods)) {
             $formattedRoute = [
                 'method'   => strtoupper($method),
-                'route'    => $args[0],
+                'route'    => self::extractRoute($args[0]),
                 'class'    => $args[1][0],
                 'callback' => $args[1][1]
             ];
@@ -62,6 +65,9 @@ class Router {
         }
     }
 
+    /**
+     * Resolves route not found 404
+     */
     private static function resolve404Page()
     {
         $routes = array_column(self::$routes, 'route');
@@ -69,5 +75,24 @@ class Router {
         $formattedRoute = self::$routes[$routeIndex];
         $instance = new $formattedRoute['class']();
         $instance->{$formattedRoute['callback']}();
+    }
+
+    /**
+     * Extract route path and route params
+     * 
+     * @param String  $route path router
+     * 
+     * @return String $path route path
+     */
+    private static function extractRoute($route)
+    {
+        $path = [];
+        if (strpos($route, '?')) {
+            $path = explode('?', $route)[0];
+        } else {
+            $path = $route;
+        }
+
+        return $path;
     }
 }
